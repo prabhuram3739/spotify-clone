cef9a4bc516048c97f4f787ca12ff213b1273d27
Prabhuram <prabhuram3739@gmail.com>
2020-08-14 12:28:55 +0530

    Added the login component, access token fetch, get the user details logged in

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [token, setToken] = useState(null);
+    //Run code based on a given condition
+    useEffect(() => {
+        const hash = getTokenFromUrl();
+        //Strip the hash from the url to not show
+        window.location.hash = "";
+        const _token = hash.access_token;
+
+        if (_token) {
+            setToken(_token);
+            //Store the access token from spotify app to this local react app to connect and use the spotify data
+            spotify.setAccessToken(_token);
+            //Get the logged in user details from the spotify api
+            spotify.getMe().then(user => {
+                console.log(user);
+            });
+        }
+        console.log('Token received from the spotify:', token);
+    }, []);
+
+    return (
+        //BEM
+        <
+        div className = "app" > {
+            token ? ( < Player / > ) : ( <
+                Login / >
+            )
+        }
+
+        <
+        /div >
+    );
 }
 
 export default App;
